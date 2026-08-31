cf2b47ffa2d20306a2d482e832f368b40467e4c9
Louis Marcellino <marcellinoharijanto@gmail.com>
2026-08-31 11:40:51 +0700

    Bundle p5 for static PlaySpace

---
+++ creators/louism/PlaySpace/vendor/p5_fallback.js
@@ -0,0 +1,5 @@
+if (typeof window.p5 == "undefined") {
+  document.write(
+    '<script src="https://cdn.jsdelivr.net/npm/p5@1.11.13/lib/p5.min.js"></script>',
+  );
+}
---
+++ creators/louism/PlaySpace/vendor/p5_sound_fallback.js
@@ -0,0 +1,5 @@
+if (typeof window.p5?.SoundFile == "undefined") {
+  document.write(
+    '<script src="https://cdn.jsdelivr.net/npm/p5@1.11.13/lib/addons/p5.sound.min.js"></script>',
+  );
+}
